9c5ce6a878a3af860b4ded98ec012814b6644874
Ryuto-Nakano <Ryuto.Nakano@ibm.com>
2024-08-01 13:26:01 +0900

    First Commit

---
+++ routes/discovery.js
@@ -0,0 +1,86 @@
+const express = require("express");
+
+const DiscoveryV2 = require("ibm-watson/discovery/v2");
+const { IamAuthenticator } = require("ibm-watson/auth");
+const config = require("config");
+
+// eslint-disable-next-line new-cap
+const router = express.Router();
+
+// 接続情報
+const discovery = new DiscoveryV2({
+  version: config.get("watson.discovery.version"),
+  authenticator: new IamAuthenticator({
+    apikey: config.get("watson.discovery.apikey"),
+  }),
+  serviceUrl: config.get("watson.discovery.serviceUrl"),
+});
+
+//const createQuery = (categoryLabel, searchStr) => {
+const createQuery = (searchStr) => {
+  const texts = searchStr
+    .split(" ")
+    .map((item) => `text:"${item}"`)
+    .join(",");
+  //return `enriched_text.categories.label::"${categoryLabel}",(${texts})`;
+  return `${texts}`;
+};
+
+const runQuery = async (categoryLabel, searchStr) => {
+  const query = createQuery(categoryLabel, searchStr);
+
+  const queryParams = {
+    projectId: config.get("watson.discovery.projectId"),
+    query,
+    highlight: true,
+    // passages: { per_document: false },
+  };
+
+  console.log(`Running query - ${query}`);
+  const queryResponse = await discovery.query(queryParams);
+
+  const results = queryResponse.result.results;
+
+  console.log(JSON.stringify(results, null, 2));
+  if (queryResponse.result.results && queryResponse.result.results.length > 0) {
+    const results = queryResponse.result.results;
+    // 変数名あとで考える
+    const responseArray = results.map((x) => {
+      const filename = x.extracted_metadata.filename
+      .replace(/<em>/g, "")
+      .replace(/<\/em>/g, "");
+      const passage = x.document_passages[0].passage_text;
+      return {
+        filename: filename,
+        passage: passage,
+      };
+    });
+    return responseArray;
+
+    // const textArray = queryResponse.result.results[0].text
+    // const filtered = textArray.map((text) => {
+    //   return text.replace(/<em>/g, '').replace(/<\/em>/g, '');
+    // });
+    // return filtered;
+  } else {
+    return "該当する情報が見つかりませんでした。";
+  }
+};
+
+router.post("/search", async (req, res) => {
+  try {
+    if (!req.body.searchText) {
+      res.status(400).send("Missing search text.");
+      return;
+    }
+
+    //const responseText = await runQuery('/health and fitness/disease', req.body.searchText);
+    const responseArray = await runQuery(req.body.searchText);
+    res.json(responseArray);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Failed to call watson service");
+  }
+});
+
+module.exports = router;
---
+++ tools/call-discovery.js
@@ -0,0 +1,52 @@
+const DiscoveryV2 = require("ibm-watson/discovery/v2");
+const { IamAuthenticator } = require("ibm-watson/auth");
+const config = require("config");
+
+const discovery = new DiscoveryV2({
+  version: config.get("watson.discovery.version"),
+  authenticator: new IamAuthenticator({
+    apikey: config.get("watson.discovery.apikey"),
+  }),
+  serviceUrl: config.get("watson.discovery.serviceUrl"),
+});
+
+// const createQuery = (categoryLabel, searchStr) => {
+//   const texts = searchStr.split(' ').map((item) => `text:"${item}"`).join(',');
+//   return `enriched_text.categories.label::"${categoryLabel}",(${texts})`;
+// };
+
+const runQuery = async (searchStr) => {
+  // const query = createQuery(categoryLabel, searchStr);
+
+  const queryParams = {
+    projectId: config.get("watson.discovery.projectId"),
+    collectionIds: [config.get("watson.discovery.collectionId")],
+    query: searchStr,
+  };
+
+  console.log(`Running query - ${searchStr}`);
+  const queryResponse = await discovery.query(queryParams);
+
+  if (queryResponse.result.results && queryResponse.result.results.length > 0) {
+    return queryResponse.result.results.map((result, index) => {
+      return { id: index + 1, name: result.extracted_metadata.title };
+    });
+  } else {
+    return [];
+  }
+};
+
+const exec = async () => {
+  try {
+    const responseText = await runQuery(
+      "/health and fitness/disease",
+      "コロナ"
+    );
+
+    console.log(responseText);
+  } catch (err) {
+    console.log("error:", err);
+  }
+};
+
+exec();
